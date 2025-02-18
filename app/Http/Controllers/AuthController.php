fa0f41286c2d214adf948f4d8801061b795ccbf2
Maneva <randriamalalamaneva@gmail.com>
2025-02-18 22:55:54 +0300

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -19,7 +20,7 @@ class AuthController extends Controller
         $validated = Validator::make($request->all(),[
             'name'=>'required|string|max:255',
             'email'=>'required|string|max:255|unique:users|email',
-            'password'=>'required|string|max:6|confirmed',
+            'password'=>'required|string|min:6|confirmed',
         ]); 
         if ($validated->fails()) {
            return response()->json($validated->errors(),422);
@@ -37,8 +38,35 @@ class AuthController extends Controller
                 'user'=>$user
             ],200);
         } catch (\Exception $e) {
-            return response()->json(['errors'=>$e->getMessage()],500);
+            return response()->json(['errors'=>$e->getMessage()],403);
         }
         
+    }
+    public function login (Request $request){
+        $validated = Validator::make($request->all(),[
+            'email'=>'required|string|max:255|email',
+            'password'=>'required|string|min:6',
+        ]);
+        if ($validated->fails()) {
+            return response()->json($validated->errors());
+        }
+        $credentials= [
+            'email'=>$request->email,
+            'password'=>$request->password
+        ];
+        try {
+            if (!Auth::attempt($credentials)) {
+               return response()->json(['errors'=>"Invalid Credential"],403);
+            } 
+            $user = User::where('email',$request->email)->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'access_token'=>$token,
+                'user'=>$user,
+            ]);
+            
+        } catch (\Exception $e) {
+            return response()->json(['errors',$e->getMessage()],403);
+        }
     }
 }
